306506445cc7c3e311706132014f4273411e93c9
stillnurs <noorsultan.mamataliev@gmail.comgit cinfig --global user.name stillnursgit config --global user.email noorsultan.mamataliev@gmail.com>
2020-08-28 20:40:53 +0600

    created new models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Product
+
 
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug', 'created', 'status', 'price']
+	prepopulated_fields = {'slug': ('name',)}
+
+
