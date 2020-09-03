fb743d5869f4a27d1c7f8b91aa1e3278c2700519
stillnurs <noorsultan.mamataliev@gmail.comgit cinfig --global user.name stillnursgit config --global user.email noorsultan.mamataliev@gmail.com>
2020-09-03 13:10:21 +0600

    added new functions (cart_add/remove/detail) to cart/views.py

--- cart/forms.py
+++ cart/forms.py
@@ -4,6 +4,6 @@ from django.forms import HiddenInput
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
-class CardAddProductForm(forms.Form):
+class CartAddProductForm(forms.Form):
 	quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
 	update = forms.BooleanField(required=False, initial=False, widget=HiddenInput)
